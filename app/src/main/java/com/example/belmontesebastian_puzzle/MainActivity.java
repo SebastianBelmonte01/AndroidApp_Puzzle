60678a9079b2420920b3158512a3750cf124d7b5
Sebastian Francisco Belmonte Cervero <sebastian.belmonte01@gmail.com>
2022-02-25 08:33:31 -0400

    'Finished'

--- app/src/main/java/com/example/belmontesebastian_puzzle/MainActivity.java
+++ app/src/main/java/com/example/belmontesebastian_puzzle/MainActivity.java
@@ -1,20 +1,55 @@
 package com.example.belmontesebastian_puzzle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv0, tv1, tv2, tv3, tv4, tv5, tv6, tv7, tv8;
+    TextView tvMov;
+    Button btnExit, btnMix, btnNew;
+    String currentUrl;
+    Bitmap img;
+    ImageView imgView;
+    RequestQueue queue;
+    Vector<Piece> pieces = new Vector<Piece>();
+    Puzzle initialPuzzle;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
 
         tv0 = findViewById(R.id.tvFirst);
         tv1 = findViewById(R.id.tvSecond);
@@ -25,6 +60,17 @@ public class MainActivity extends AppCompatActivity {
         tv6 = findViewById(R.id.tvSeventh);
         tv7 = findViewById(R.id.tvEighth);
         tv8 = findViewById(R.id.tvNinth);
+        tvMov = findViewById(R.id.tvMov);
+        btnExit = findViewById(R.id.btnSalir);
+        btnMix = findViewById(R.id.btnMezclar);
+        btnNew = findViewById(R.id.btnNuevo);
+
+        imgView = findViewById(R.id.ivPic);
+
+
+
+
+
 
 
         Piece piece0 = new Piece(0,tv0);
@@ -37,7 +83,6 @@ public class MainActivity extends AppCompatActivity {
         Piece piece7 = new Piece(7,tv7);
         Piece piece8 = new Piece(8,tv8);
 
-        Vector<Piece> pieces = new Vector<Piece>();
         pieces.add(piece0);
         pieces.add(piece1);
         pieces.add(piece2);
@@ -48,16 +93,101 @@ public class MainActivity extends AppCompatActivity {
         pieces.add(piece7);
         pieces.add(piece8);
 
-        Puzzle initialPuzzle = new Puzzle(pieces);
-        initialPuzzle.printPuzzle();
-        Puzzle auxPuzzle = initialPuzzle;
-        initialPuzzle.mixing();
-        initialPuzzle.printPuzzle();
+        queue = Volley.newRequestQueue(this);
 
-        initialPuzzle.clickReceived();
+        queue.add(requestImage());
 
 
 
+
+        /*
+        Bitmap squareImage =  Bitmap.createBitmap(img, 250, 250, 250, 250);
+        Drawable d = new BitmapDrawable(Resources.getSystem(),squareImage);
+        imgView.setBackground(d);
+         */
+
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initialPuzzle.mixing(tvMov);
+            }
+        });
+
+        btnNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
+
+    public ImageRequest requestImage(){
+
+        currentUrl = "https://picsum.photos/750";
+
+        ImageRequest req = new ImageRequest(
+                currentUrl,
+                new Response.Listener<Bitmap>(){
+                    @Override
+                     public void onResponse(Bitmap response){
+                        System.out.println(response.toString());
+                        img = response;
+                        System.out.println("IMGA: " + img.getHeight() + " " + img.getWidth());
+
+                        Drawable background = new BitmapDrawable(getResources(),img);
+                        imgView.setBackground(background);
+                        cutImage(img);
+
+                        initialPuzzle = new Puzzle(pieces);
+                        initialPuzzle.mixing(tvMov);
+                        initialPuzzle.clickReceived(tvMov, MainActivity.this);
+                    }
+                },
+                750,
+                750,
+                Bitmap.Config.RGB_565,
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        Log.e("aa","Error"+error.getMessage());
+                    }
+                }
+
+        );
+        req.setShouldCache(false);
+        return req;
+    }
+    public void cutImage(Bitmap background) {
+        int y = 0;
+        for (int i = 0; i < 3; i++) {
+            int x = 0;
+            for(int j = 0; j < 3; j++){
+                Piece current = pieces.get(( 3 * i ) + j);
+                current.getTextView().setWidth(250);
+                current.getTextView().setHeight(250);
+                if(current.getIndex() != 8){
+                    Bitmap squareImage =  Bitmap.createBitmap(background, x, y, 250, 250);
+                    Drawable d = new BitmapDrawable(getResources(),squareImage);
+                    System.out.println("D: " + d);
+
+                    current.getTextView().setBackground(d);
+                }
+                x += 250;
+            }
+            y += 250;
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/belmontesebastian_puzzle/Puzzle.java
+++ app/src/main/java/com/example/belmontesebastian_puzzle/Puzzle.java
@@ -1,6 +1,17 @@
 package com.example.belmontesebastian_puzzle;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -8,9 +19,20 @@ import java.util.Vector;
 
 public class Puzzle {
     public Vector<Piece> puzzle;
+    int c = 0;
+    TextView tvMovement;
+    public Drawable blackBackground;
+
+
+
+
+    private Vector <Drawable> backgrounds;
 
     public Puzzle(Vector<Piece> puzzle) {
         this.puzzle = puzzle;
+        blackBackground = puzzle.get(8).getTextView().getBackground();
+        backgrounds = savingBackgrounds();
+
     }
 
     public Vector<Piece> getPuzzle() {
@@ -21,59 +43,73 @@ public class Puzzle {
         this.puzzle = puzzle;
     }
 
-    public void clickReceived() {
+    public void clickReceived(TextView tvMov, MainActivity m) {
         for(int i = 0; i < this.puzzle.size(); i++) {
             final int ii = i;
             puzzle.get(i).getTextView().setOnClickListener(new View.OnClickListener() {
                @Override
                 public void onClick(View v) {
+                   c++;
+                   tvMov.setText(String.valueOf(c));
                    Piece clickedPiece = puzzle.get(ii);
+
                    System.out.println("Click in piece with index: " + ii );
                    System.out.println(ii);
+
                    if((ii < 2  || ii < 5 || ii < 8) && puzzle.get(ii + 1).getTextView().getText().toString().length() == 0){
                        System.out.println("El de mi derecha es vacio");
                        puzzle.get(ii).exchangedPieces(ii, ii+1,puzzle);
                    }
                    else{
                        if((ii > 0 || ii > 3 || ii > 6) && puzzle.get(ii - 1).getTextView().getText().toString().length() == 0){
-                           System.out.println("El de mi izquierda es vacio");
                            puzzle.get(ii).exchangedPieces(ii, ii-1,puzzle);
                        }
                        else{
-                           if(ii - 3 > 0 && puzzle.get(ii - 3).getTextView().getText().length() == 0){
+                           if(ii - 3 >= 0 && puzzle.get(ii - 3).getTextView().getText().length() == 0){
                                puzzle.get(ii).exchangedPieces(ii, ii-3,puzzle);
                            }
-                           else if(ii + 3 > 0 && puzzle.get(ii + 3).getTextView().getText().length() == 0){
+                           else if(ii + 3 > 0 && ii != 8 && puzzle.get(ii + 3).getTextView().getText().length() == 0){
                                puzzle.get(ii).exchangedPieces(ii, ii+3,puzzle);
-
                            }
                        }
                    }
-                   printPuzzle();
+                    if(!isFinished()){
+                        System.out.println("No terminado");
+                    }
+                    else{
+                        System.out.println("Terminado");
+                        finishedMessage(m);
+                    }
                }
 
             });
 
         }
-
     }
 
-    public void mixing(){
+    public void mixing(TextView tvMov){
+        tvMovement = tvMov;
+        c=0;
+        tvMovement.setText(String.valueOf(c));
         ArrayList<Integer> mixedIndexes = generateIntSet();
+        Drawable black = this.puzzle.get(8).getTextView().getBackground();
 
         for(int i = 0; i < this.puzzle.size(); i++){
-            //TODO creo que si solo se cambian los inidices se cambia todo idk
-
             this.puzzle.get(i).setIndex(mixedIndexes.get(i));
             //System.out.println("Nuevo index en: " + this.puzzle.get(i).getTextView().getText() );
 
 
-            System.out.println("Indice inicial: " + i + " Indice nuevo: "  + mixedIndexes.get(i));
             System.out.println(this.puzzle.get(mixedIndexes.get(i)).getTextView().getText());
-            if(this.puzzle.get(i).getTextView().getText().length() != 0){
+           if(this.puzzle.get(i).getIndex() != 8){
                 this.puzzle.get(i).getTextView().setText(String.valueOf(mixedIndexes.get(i)));
 
-            }
+           }
+           else{
+               this.puzzle.get(i).getTextView().setText("");
+               this.puzzle.get(i).getTextView().setBackground(blackBackground);
+           }
+           this.puzzle.get(i).getTextView().setBackground(backgrounds.get(this.puzzle.get(i).getIndex()));
+
         }
 
     }
@@ -102,4 +138,46 @@ public class Puzzle {
         System.out.println(buffer);
 
     }
+
+    public boolean isFinished() {
+        for (int i = 0; i < this.puzzle.size(); i++) {
+            if (this.puzzle.get(i).getIndex() != i) {
+                System.out.println("NOT FINISHED");
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    private Vector<Drawable> savingBackgrounds(){
+        Vector<Drawable> backgrounds = new Vector<Drawable>();
+        for(int i = 0; i < this.puzzle.size(); i ++){
+                backgrounds.add(this.puzzle.get(i).getTextView().getBackground());
+        }
+        return backgrounds;
+    }
+    public void finishedMessage(MainActivity m){
+        AlertDialog.Builder builder = new AlertDialog.Builder(m);
+        builder.setCancelable(false);
+        builder.setTitle("YOU WIN!");
+        builder.setMessage("Completaste el juego\n¿Deseas comenzar de nuevo?");
+
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                mixing(tvMovement);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(m,StartActivity.class);
+                m.startActivity(intent);
+            }
+        });
+        builder.show();
+    }
+
+
 }
